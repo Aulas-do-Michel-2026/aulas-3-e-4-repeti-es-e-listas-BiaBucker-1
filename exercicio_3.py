636bc9933ab77de053d8131bb34451a1c91cc3da
Beatriz Bucker <beatrizbucker.tralhao@einstein.edu.br>
2026-05-24 19:50:54 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -23,7 +23,6 @@ Os números ímpares são [1, 3, 5]
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui...
-lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
 lista_impares =[]
 
